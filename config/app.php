5742f6826e01ce3eb592f4bbb8246c2ff2cda327
Peter Takacs <peter.takacs@koodilab.com>
2017-08-19 18:44:19 +0200

    Installed the Laravel Passport

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Laravel\Passport\PassportServiceProvider::class,
 
         /*
          * Application Service Providers...
