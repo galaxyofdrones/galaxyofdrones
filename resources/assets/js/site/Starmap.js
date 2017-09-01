73bc81ad784620e93962e4e619e88cae18134432
Peter Takacs <peter.takacs@koodilab.com>
2017-09-02 00:15:31 +0200

    Added the geo json layer

--- resources/assets/js/site/Starmap.js
+++ resources/assets/js/site/Starmap.js
@@ -4,7 +4,7 @@ export default {
     data() {
         return {
             map: undefined,
-            zoom: 0,
+            zoom: 0
         };
     },
 
@@ -16,7 +16,83 @@ export default {
         this.initLeaflet();
     },
 
-    computed: {
+    methods: {
+        initLeaflet() {
+            L.Icon.Default.imagePath = `${this.imagePath}/`;
+
+            this.map = L.map(this.$el, {
+                attributionControl: false,
+                boxZoom: false,
+                crs: L.CRS.Simple,
+                minZoom: 0,
+                maxZoom: this.maxZoom,
+                zoomControl: false
+            });
+
+            this.map.setView(this.center(), this.maxZoom);
+
+            L.tileLayer(this.tileUrl, {
+                noWrap: true,
+                bounds: L.latLngBounds(this.southWest(), this.northEast()),
+            }).addTo(this.map);
+
+            const geoJsonLayer = L.geoJson.ajax(this.geoJson(), {
+                pointToLayer: (geoJsonPoint, latlng) => {
+                    const point = this.map.unproject([
+                        latlng.lng, latlng.lat
+                    ], this.maxZoom);
+
+                    const size = (geoJsonPoint.properties.size + 16) / this.multiplier();
+
+                    const options = {
+                        className: geoJsonPoint.properties.type === 'planet'
+                            ? `leaflet-div-icon ${geoJsonPoint.properties.status}`
+                            : 'leaflet-div-icon',
+                        iconSize: [
+                            size, size
+                        ]
+                    };
+
+                    if (this.map.getZoom() >= 8) {
+                        options.html = `<span>${geoJsonPoint.properties.name}</span>`;
+                    }
+
+                    const marker = L.marker(point, {
+                        title: geoJsonPoint.properties.name,
+                        icon: L.divIcon(options)
+                    });
+
+                    marker.on('click', () => {});
+
+                    return marker;
+                }
+            });
+
+            geoJsonLayer.ajaxParams.headers = {
+                'X-Requested-With': 'XMLHttpRequest',
+                'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content
+            };
+
+            geoJsonLayer.addTo(this.map);
+
+            this.map.on('zoomstart', () => geoJsonLayer.clearLayers());
+            this.map.on('moveend', () => geoJsonLayer.refresh(this.geoJson()));
+        },
+
+        geoJson() {
+            const bounds = this.map.getPixelBounds();
+            const multiplier = this.multiplier();
+
+            return this.geoJsonUrl
+                .replace('__zoom__', this.map.getZoom())
+                .replace('__bounds__', [
+                    bounds.min.x * multiplier,
+                    bounds.min.y * multiplier,
+                    bounds.max.x * multiplier,
+                    bounds.max.y * multiplier
+                ].join(','));
+        },
+
         center() {
             return this.map.unproject([
                 this.size / 2, this.size / 2
@@ -33,28 +109,10 @@ export default {
             return this.map.unproject([
                 this.size, this.size
             ], this.zoom);
-        }
-    },
+        },
 
-    methods: {
-        initLeaflet() {
-            L.Icon.Default.imagePath = this.imagePath;
-
-            this.map = L.map(this.$el, {
-                attributionControl: false,
-                boxZoom: false,
-                crs: L.CRS.Simple,
-                minZoom: 0,
-                maxZoom: this.maxZoom,
-                zoomControl: false
-            });
-
-            this.map.setView(this.center, this.maxZoom);
-
-            L.tileLayer(this.tileUrl, {
-                noWrap: true,
-                bounds: L.latLngBounds(this.southWest, this.northEast),
-            }).addTo(this.map);
+        multiplier() {
+            return Math.pow(2, this.maxZoom - this.map.getZoom());
         }
     }
 };
