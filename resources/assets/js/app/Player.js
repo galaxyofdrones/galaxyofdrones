983f31fc3f2ca4cd4cda48ba65131d6939d7dd91
Peter Takacs <peter.takacs@koodilab.com>
2018-03-04 17:20:05 +0100

    Fixed the template loading

--- resources/assets/js/app/Player.js
+++ resources/assets/js/app/Player.js
@@ -13,6 +13,7 @@ export default {
                 username: '',
                 production_rate: 0,
                 experience: 0,
+                level: 1,
                 level_experience: 0,
                 next_level_experience: 0,
                 notification_count: 0
