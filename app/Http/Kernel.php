61d7c48a56d8f9ab934bcba55ab99cc3cc1f46ce
Peter Takacs <peter.takacs@koodilab.com>
2018-10-13 16:55:24 +0200

    Registered the passport middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,6 +29,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Koodilab\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
         ],
 
         'api' => [
