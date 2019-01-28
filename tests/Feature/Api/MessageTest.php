9f26926e9f30fefeab3085b8fbf4e7ca3aa43391
mikecpl <mikecorporal@gmail.com>
2019-01-28 23:59:37 +0100

    Added message factory and unit test.

---
+++ tests/Feature/Api/MessageTest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Carbon\Carbon;
+use Illuminate\Contracts\Notifications\Dispatcher;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Validator;
+use Koodilab\Http\Middleware\VerifyCsrfToken;
+use Koodilab\Http\Requests\Api\MessageStoreRequest;
+use Koodilab\Models\Expedition;
+use Koodilab\Models\Message;
+use Koodilab\Models\Star;
+use Koodilab\Models\Unit;
+use Koodilab\Models\User;
+use Koodilab\Notifications\MessageSended;
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+
+class MessageTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $user = factory(User::class)->create([
+            'started_at' => Carbon::now(),
+        ]);
+
+        Passport::actingAs($user);
+
+        $sender = factory(User::class)->create([
+            'id' => 2,
+            'username' => 'Mike',
+            'started_at' => Carbon::now(),
+        ]);
+
+        $message = factory(Message::class)->create([
+            'id' => 1,
+            'message' => 'Test message',
+            'recipient_id' => $user->id,
+            'sender_id' => $sender->id,
+            'created_at' => Carbon::create(2018, 1, 1, 19),
+        ]);
+
+        Notification::fake();
+
+        $user->notify(new MessageSended($message->id));
+
+        Notification::assertSentTo($user, MessageSended::class);
+    }
+
+    public function testIndex()
+    {
+        $user = auth()->user();
+        $message = $user->messages()->first();
+
+        $this->getJson('/api/message')->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    [
+                        'id',
+                        'message',
+                        'created_at',
+                        'sender' => [
+                            'id',
+                            'username',
+                            'isBlocked',
+                            'isBlockedBy',
+                        ],
+                    ],
+                ],
+            ])->assertJson([
+                'data' => [
+                    [
+                        'id' => 1,
+                        'message' => 'Test message',
+                        'created_at' => Carbon::create(2018, 1, 1, 19),
+                        'sender' => [
+                            'id' => 2,
+                            'username' => 'Mike',
+                            'isBlocked' => false,
+                            'isBlockedBy' => false,
+                        ],
+                    ],
+                ],
+            ]);
+    }
+
+    public function testStore()
+    {
+        $user = auth()->user();
+
+        $this->assertDatabaseMissing('messages', [
+            'sender_id' => $user->id,
+        ]);
+
+        $this->post('/api/message', [
+            'recipient' => 'Mike',
+            'message' => 'Test message',
+        ])->assertStatus(200);
+
+        $this->assertDatabaseHas('messages', [
+            'sender_id' => $user->id,
+        ]);
+    }
+}
