2b2966e251698ae1e983b21fe5338356caac4d50
mikecpl <mikecorporal@gmail.com>
2019-02-11 20:09:18 +0100

    Added expedition log unit test and factory.

---
+++ database/factories/ExpeditionLogFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/* @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(Koodilab\Models\ExpeditionLog::class, function (Faker $faker) {
+    return [
+        'star_id' => function () {
+            return factory(Koodilab\Models\Star::class)->create()->id;
+        },
+        'user_id' => function () {
+            return factory(Koodilab\Models\User::class)->create()->id;
+        },
+        'experience' => $faker->numberBetween(1, 100),
+        'solarion' => $faker->numberBetween(1, 50),
+    ];
+});
