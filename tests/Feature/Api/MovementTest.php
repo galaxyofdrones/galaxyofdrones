c832213ba5eabece8135efacbf47ce802e6cc3f7
mikecpl <mikecorporal@gmail.com>
2019-02-20 18:00:08 +0100

    Added support and transport unit tests.

--- tests/Feature/Api/MovementTest.php
+++ tests/Feature/Api/MovementTest.php
@@ -11,6 +11,7 @@ use Koodilab\Models\Movement;
 use Koodilab\Models\Planet;
 use Koodilab\Models\Population;
 use Koodilab\Models\Resource;
+use Koodilab\Models\Stock;
 use Koodilab\Models\Unit;
 use Koodilab\Models\User;
 use Laravel\Passport\Passport;
@@ -221,6 +222,150 @@ class MovementTest extends TestCase
         $this->assertDatabaseHas('movements', [
             'user_id' => $user->id,
             'type' => Movement::TYPE_OCCUPY,
+        ]);
+    }
+
+    public function testStoreSupport()
+    {
+        $user = auth()->user();
+
+        $this->post('/api/movement/support/10')
+            ->assertStatus(404);
+
+        $this->post('/api/movement/support/not-id')
+            ->assertStatus(404);
+
+        $planet = factory(Planet::class)->create([
+            'user_id' => null,
+        ]);
+
+        $this->post("/api/movement/support/{$planet->id}")
+            ->assertStatus(403);
+
+        $unit = factory(Unit::class)->create([
+            'speed' => 100,
+        ]);
+
+        $population = factory(Population::class)->create([
+            'planet_id' => $user->current_id,
+            'unit_id' => $unit->id,
+            'quantity' => 5,
+        ]);
+
+        $planet = factory(Planet::class)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $this->post("/api/movement/support/{$planet->id}", [
+            'quantity' => [
+                $unit->id => 10,
+            ],
+        ])->assertStatus(400);
+
+        $population->update([
+            'quantity' => 20,
+        ]);
+
+        $this->assertDatabaseMissing('movements', [
+            'user_id' => $user->id,
+            'type' => Movement::TYPE_SUPPORT,
+        ]);
+
+        Bus::fake();
+
+        $this->post("/api/movement/support/{$planet->id}", [
+            'quantity' => [
+                $unit->id => 10,
+            ],
+        ])->assertStatus(200);
+
+        Bus::assertDispatched(Move::class);
+
+        $this->assertDatabaseHas('movements', [
+            'user_id' => $user->id,
+            'type' => Movement::TYPE_SUPPORT,
+        ]);
+    }
+
+    public function testStoreTransport()
+    {
+        $user = auth()->user();
+
+        $this->post('/api/movement/transport/10')
+            ->assertStatus(404);
+
+        $this->post('/api/movement/transport/not-id')
+            ->assertStatus(404);
+
+        $planet = factory(Planet::class)->create([
+            'user_id' => null,
+        ]);
+
+        $this->post("/api/movement/transport/{$planet->id}")
+            ->assertStatus(403);
+
+        $unit = factory(Unit::class)->create([
+            'type' => Unit::TYPE_TRANSPORTER,
+            'speed' => 100,
+            'capacity' => 10,
+        ]);
+
+        $resource = factory(Resource::class)->create();
+
+        $population = factory(Population::class)->create([
+            'planet_id' => $user->current_id,
+            'unit_id' => $unit->id,
+            'quantity' => 5,
+        ]);
+
+        $planet = factory(Planet::class)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $stock = factory(Stock::class)->create([
+            'planet_id' => $user->current_id,
+            'resource_id' => $resource->id,
+            'quantity' => 5,
+        ]);
+
+        $this->post("/api/movement/transport/{$planet->id}", [
+            'quantity' => [
+                $resource->id => 10,
+            ],
+        ])->assertStatus(400);
+
+        $population->update([
+            'quantity' => 20,
+        ]);
+
+        $this->post("/api/movement/transport/{$planet->id}", [
+            'quantity' => [
+                $resource->id => 10,
+            ],
+        ])->assertStatus(400);
+
+        $stock->update([
+            'quantity' => 20,
+        ]);
+
+        $this->assertDatabaseMissing('movements', [
+            'user_id' => $user->id,
+            'type' => Movement::TYPE_TRANSPORT,
+        ]);
+
+        Bus::fake();
+
+        $this->post("/api/movement/transport/{$planet->id}", [
+            'quantity' => [
+                $resource->id => 10,
+            ],
+        ])->assertStatus(200);
+
+        Bus::assertDispatched(Move::class);
+
+        $this->assertDatabaseHas('movements', [
+            'user_id' => $user->id,
+            'type' => Movement::TYPE_TRANSPORT,
         ]);
     }
 }
