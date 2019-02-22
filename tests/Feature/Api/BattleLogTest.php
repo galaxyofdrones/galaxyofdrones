65262f5dce94fb3213f856391d76c9fc3a552ca5
mikecpl <mikecorporal@gmail.com>
2019-02-23 00:19:56 +0100

    Enhanced the batte log unit test.

--- tests/Feature/Api/BattleLogTest.php
+++ tests/Feature/Api/BattleLogTest.php
@@ -5,6 +5,9 @@ namespace Tests\Feature\Api;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Koodilab\Models\BattleLog;
+use Koodilab\Models\Building;
+use Koodilab\Models\Resource;
+use Koodilab\Models\Unit;
 use Koodilab\Models\User;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
@@ -22,28 +25,45 @@ class BattleLogTest extends TestCase
         ]);
 
         Passport::actingAs($user);
-
-        $battleLog = factory(BattleLog::class)->create([
-            'attacker_id' => $user->id,
-        ]);
     }
 
     public function testIndex()
     {
+        $user = auth()->user();
+
+        $battleLog = factory(BattleLog::class)->create([
+            'attacker_id' => $user->id,
+        ]);
+
+        $resource = factory(Resource::class)->create();
+        $battleLog->resources()->attach($resource, [
+            'quantity' => 10,
+            'losses' => 5,
+        ]);
+
+        $building = factory(Building::class)->create();
+        $battleLog->buildings()->attach($building, [
+            'level' => 10,
+            'losses' => 5,
+        ]);
+
+        $attackerUnit = factory(Unit::class)->create();
+        $battleLog->attackerUnits()->attach($attackerUnit, [
+            'owner' => BattleLog::OWNER_ATTACKER,
+            'quantity' => 10,
+            'losses' => 5,
+        ]);
+
+        $defenderUnit = factory(Unit::class)->create();
+        $battleLog->defenderUnits()->attach($defenderUnit, [
+            'owner' => BattleLog::OWNER_DEFENDER,
+            'quantity' => 10,
+            'losses' => 5,
+        ]);
+
         $this->getJson('/api/battle-log')
             ->assertStatus(200)
             ->assertJsonStructure([
-                'current_page',
-                'first_page_url',
-                'from',
-                'last_page',
-                'last_page_url',
-                'next_page_url',
-                'path',
-                'per_page',
-                'prev_page_url',
-                'to',
-                'total',
                 'data' => [
                     [
                         'id',
@@ -70,10 +90,107 @@ class BattleLogTest extends TestCase
                             'id',
                             'username',
                         ],
-                        'resources',
-                        'buildings',
-                        'attacker_units',
-                        'defender_units',
+                        'resources' => [
+                            [
+                                'id',
+                                'name',
+                                'description',
+                                'quantity',
+                                'losses',
+                            ]
+                        ],
+                        'buildings' => [
+                            [
+                                'id',
+                                'name',
+                                'description',
+                                'level',
+                                'losses',
+                            ]
+                        ],
+                        'attacker_units' => [
+                            [
+                                'id',
+                                'name',
+                                'description',
+                                'quantity',
+                                'losses',
+                            ]
+                        ],
+                        'defender_units' => [
+                            [
+                                'id',
+                                'name',
+                                'description',
+                                'quantity',
+                                'losses',
+                            ]
+                        ],
+                    ],
+                ],
+            ])->assertJson([
+                'data' => [
+                    [
+                        'id' => $battleLog->id,
+                        'type' => $battleLog->type,
+                        'winner' => $battleLog->winner,
+                        'created_at' => $battleLog->created_at->toDateTimeString(),
+                        'is_attack' => true,
+                        'is_defense' => false,
+                        'start' => [
+                            'id' => $battleLog->start_id,
+                            'resource_id' => $battleLog->start->resource_id,
+                            'name' => $battleLog->start_name,
+                        ],
+                        'end' => [
+                            'id' => $battleLog->end_id,
+                            'resource_id' => $battleLog->end->resource_id,
+                            'name' => $battleLog->end_name,
+                        ],
+                        'attacker' => [
+                            'id' => $battleLog->attacker_id,
+                            'username' => $battleLog->attacker->username,
+                        ],
+                        'defender' => [
+                            'id' => $battleLog->defender_id,
+                            'username' => $battleLog->defender->username,
+                        ],
+                        'resources' => [
+                            [
+                                'id' => $resource->id,
+                                'name' => $resource->translation('name'),
+                                'description' => $resource->translation('description'),
+                                'quantity' => 10,
+                                'losses' => 5,
+                            ]
+                        ],
+                        'buildings' => [
+                            [
+                                'id' => $building->id,
+                                'name' => $building->translation('name'),
+                                'description' => $building->translation('description'),
+                                'level' => 10,
+                                'losses' => 5,
+                            ]
+                        ],
+                        'attacker_units' => [
+                            [
+                                'id' => $attackerUnit->id,
+                                'name' => $attackerUnit->translation('name'),
+                                'description' => $attackerUnit->translation('description'),
+                                'quantity' => 10,
+                                'losses' => 5,
+                            ]
+                        ],
+                        'defender_units' => [
+                            [
+                                'id' => $defenderUnit->id,
+                                'name' => $defenderUnit->translation('name'),
+                                'description' => $defenderUnit->translation('description'),
+                                'quantity' => 10,
+                                'losses' => 5,
+                            ]
+                        ],
                     ],
                 ],
             ]);
