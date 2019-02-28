5aca8f95dc1cb4faf009979f7a63b62e7f03416b
Peter Takacs <peter.takacs@koodilab.com>
2019-02-28 17:35:33 +0100

    Fixed the build

--- app/Models/Queries/FindByIdOrUsername.php
+++ app/Models/Queries/FindByIdOrUsername.php
@@ -10,7 +10,7 @@ trait FindByIdOrUsername
      * @param mixed $value
      * @param array $columns
      *
-     * @return \Illuminate\Database\Eloquent\Model|null|static
+     * @return \Illuminate\Database\Eloquent\Model|static|null
      */
     public static function findByIdOrUsername($value, $columns = ['*'])
     {
