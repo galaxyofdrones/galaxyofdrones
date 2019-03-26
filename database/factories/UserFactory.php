0b0cade3a683452ac14bdb1b40854ad3d40628b1
Peter Takacs <peter.takacs@koodilab.com>
2019-03-26 20:06:05 +0100

    CS fixes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,13 +1,15 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use Koodilab\Models\User;
 
 /* @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(Koodilab\Models\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     return [
         'username' => $faker->userName,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'remember_token' => Str::random(10),
     ];
 });
